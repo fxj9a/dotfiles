f624135545878f998af8f0733d3da7e670f507d6
fxj9a <adrian.sprucer@gmail.com>
2020-11-12 16:24:26 +0100

    change some keybindings (fixed)

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 fish_vi_key_bindings
 set EDITOR nvim
 alias vim nvim
+alias k kaput
 function wtr
   if [ -n $argv ]
     set query "wttr.in/~$argv?0"
@@ -10,9 +11,9 @@ function wtr
   curl $query
 end
 
-function k
-  echo "Ctrl+D to exit"
-  while read -l k_mycommand -P "kaput> "
-  	kaput $k_mycommand
-  end
-end
+#function k
+#  echo "Ctrl+D to exit"
+# while read -l k_mycommand -P "kaput> "
+#  	kaput $k_mycommand
+#  end
+#end
