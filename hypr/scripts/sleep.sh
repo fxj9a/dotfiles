8f396c5b32f7bdec629d1acbff6c2aa63462714c
fxj9a <adrian.sprucer@gmail.com>
2024-02-14 21:27:02 +0100

    _ changes

--- hypr/scripts/sleep.sh
+++ hypr/scripts/sleep.sh
@@ -1,3 +1,2 @@
-swayidle -w timeout 300 'swaylock -f -c 0000FF' \
-            timeout 600 'systemctl suspend' \
-            before-sleep 'swaylock -f -c 0000FF' &
+swayidle -w timeout 600 'systemctl suspend' \
+            before-sleep 'swaylock -f -c 00333a' &
