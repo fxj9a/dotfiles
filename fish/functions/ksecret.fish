71e577064bc3f223855d3883763c85d36a13c4b1
Adrian fxj9a <adrian.sprucer@gmail.com>
2025-03-12 16:01:43 +0100

    _ Fucking egotistical bastard. adds expandtab to vimrc

--- fish/functions/ksecret.fish
+++ fish/functions/ksecret.fish
@@ -2,10 +2,19 @@ function ksecret --wraps=kubectl --description 'interactive secret from k8s deco
   kubectl config view --minify -o jsonpath='{..namespace}' | read  currentns
   kubectl get ns --no-headers -o custom-columns=":metadata.name" | fzf --prompt "Namespace: " --query="$currentns" --no-sort  | read namespace 
 
-  kubectl get secrets -n $namespace --no-headers -o custom-columns=":metadata.name" | fzf --prompt "Secret Name: " | read secretname
+  kubectl get secrets -n $namespace --no-headers -o custom-columns=":metadata.name" | fzf -0 --prompt "Secret Name: " | read secretname
+  if test -z "$secretname"
+    echo "Namespace $namespace doesn't have any secrets"
+    return 1
+  end
 
-  kubectl get secret $secretname -n $namespace -o json | jq -r '.data | keys[]' | fzf -1 --prompt "Secret Key: " | read secretkey
 
-  kubectl get secret $secretname -n $namespace -o jsonpath="{.data.$secretkey}" | base64 -d
-        
+  kubectl get secret $secretname -n $namespace -o json | jq '.data | keys[]' | fzf -0 -1 --prompt "Secret Key: " | read secretkey
+  if test -z "$secretkey"
+    echo "Secret $secretname doesn't have any secret keys"
+    return 2
+  end
+
+  kubectl get secret $secretname -n $namespace -o json | jq -r ".data.$secretkey" | base64 -d
+
 end
