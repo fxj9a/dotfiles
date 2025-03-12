bd5099359efd953e08446e709fe15c848b645bce
Adrian fxj9a <adrian.sprucer@gmail.com>
2025-03-12 11:24:46 +0100

    get k8s secret interactively with ksecret

---
+++ fish/functions/ksecret.fish
@@ -0,0 +1,11 @@
+function ksecret --wraps=kubectl --description 'interactive secret from k8s decoder'
+  kubectl config view --minify -o jsonpath='{..namespace}' | read  currentns
+  kubectl get ns --no-headers -o custom-columns=":metadata.name" | fzf --prompt "Namespace: " --query="$currentns" --no-sort  | read namespace 
+
+  kubectl get secrets -n $namespace --no-headers -o custom-columns=":metadata.name" | fzf --prompt "Secret Name: " | read secretname
+
+  kubectl get secret $secretname -n $namespace -o json | jq '.data | keys[]' | fzf -1 --prompt "Secret Key: " | tr -d \" | read secretkey
+
+  kubectl get secret $secretname -n $namespace -o jsonpath="{.data.$secretkey}" | base64 -d
+        
+end
