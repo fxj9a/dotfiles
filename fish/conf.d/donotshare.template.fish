3b2f4aaf3dbb5772358d60f2e76894d2e7ac3be6
fxj9 <fxj9@proton.me>
2026-02-13 10:41:17 +0100

    add donotshare.template.fish

---
+++ fish/conf.d/donotshare.template.fish
@@ -0,0 +1,11 @@
+# wg-quick helpers
+#abbr --add wgup "sudo wg-quick up"
+#abbr --add wwgup "sudo wg-quick up vpnname"
+#abbr --add wgup "sudo wg-quick down"
+#abbr --add wwgup "sudo wg-quick down vpnname"
+# ssh quick launchers
+#abbr --add ec2name "ssh <...>"
+# Org's container registry (AZURE_REGISTRY_NAME.azurecr.io)
+#set -x DOCKER_REGISTRY
+# name of azure container registry
+#set -x AZURE_REGISTRY_NAME
